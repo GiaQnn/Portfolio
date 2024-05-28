4933d57b25f6eb7bea5e0e71b12be25a9ed93db2
Catherine <ykatarzynabara@gmail.com>
2024-05-28 18:44:37 +0100

    if..else statements for name, question and answer variables

--- magic8.py
+++ magic8.py
@@ -4,3 +4,13 @@ name = ""
 question = ""
 answer = ""
 random_number = random.randint(1, 10)
+
+# if statements of first three variables
+if question == "" and name == "":
+  print("The Magic 8-Ball can not provide a fortune unless you as it something.")
+elif name == "":
+  print("Your question:", question)
+elif not name == "":
+  print(name, "asks:", question)
+else:
+  print(random_number)
