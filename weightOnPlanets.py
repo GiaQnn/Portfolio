2dfce3a92a996d72cb1afa9c88c46282b1cc4fbb
Catherine <ykatarzynabara@gmail.com>
2024-05-28 20:07:06 +0100

    if..else statements about weight on planets

--- weightOnPlanets.py
+++ weightOnPlanets.py
@@ -5,3 +5,19 @@ print("   4. Saturn     5. Uranus   6. Neptune\n")
 
 weight = 185
 planet = 5
+
+# Write an if statement below:
+if planet == 1:
+  weight = weight * .91
+elif planet == 2:
+  weight = weight * .38
+elif planet == 3:
+  weight = weight * 2.34
+elif planet == 4:
+  weight = weight * 1.06
+elif planet == 5:
+  weight = weight * .92
+elif planet == 6:
+  weight = weight * 1.19
+
+print("Your weight: ", weight)
